d5dce0a503bb1f8f82d210b4b02cabb2548142f7
mohammedzarhbouch <120816@vistacollege.nl>
2024-09-19 16:10:08 +0200

    oke de routes en annotations werkt HIER IS HET GOED.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
-
-    /**
-     *
-     */
 {
-    public function homepage()
+    /**
+     * @Route("/", name="home")
+     */
+    public function homepage(): Response
     {
         return $this->render('home.html.twig');
     }
