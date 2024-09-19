f53ae7e822f12a04526dbfb37fc739d23eb00f78
mohammedzarhbouch <120816@vistacollege.nl>
2024-09-19 16:33:20 +0200

    DIT WERKT DID IS JUIST

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,7 +2,17 @@
 
 namespace App\Controller;
 
-class LoginController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class LoginController extends AbstractController
+{
+    /**
+     * @Route('/login', name='login')
+     */
+    public function loginpage(): Response
+    {
+        return $this->render('login.html.twig');
+    }
 }
